edeafa3b09850ea326168ee7a42aa20d111d849d
het-codes-ai <modijignisha12@gmail.com>
2026-05-28 21:36:07 +0530

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		int a=83;
+		int b=25;
+		
+		int sum =a+b;
+		System.out.println(sum);
+		
+		int diff=a-b;
+		System.out.println(diff);	
+		
+		int prod=a*b;
+		System.out.println(prod);
+		
+		int div=a/b;
+		System.out.println(div);
+	}
+}
