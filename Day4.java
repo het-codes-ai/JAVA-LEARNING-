fb72c6ee72a7d173207886512ae464d07183f41e
het-codes-ai <modijignisha12@gmail.com>
2026-05-29 18:47:09 +0530

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("ENTER YOUR NUMBER");	
+	Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	
+	if (num%2==0){
+		System.out.println("NUMBER IS EVEN");
+		}
+	else{
+		System.out.println("NUMBER IS ODD");	
+		}
+	}
+}
