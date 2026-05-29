cf435a0a6311294221549ca7efce85f87e3ebcd8
het-codes-ai <modijignisha12@gmail.com>
2026-05-29 19:32:39 +0530

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter button number  ");
+	int button=sc.nextInt();
+	
+	switch(button){
+		case 1:
+		{System.out.println("Hello");
+		break;
+		}
+		case 2:
+		{System.out.println("Namaste");
+		break;
+			}
+		case 3:
+		{System.out.println("Pranam");
+		break;
+			}
+		default:
+		{System.out.println("Invalid button");}
+	}
+	}
+}
