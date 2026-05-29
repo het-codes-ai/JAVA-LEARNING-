86907e63d9eb605057f6783ff3185e6558cb5d10
het-codes-ai <modijignisha12@gmail.com>
2026-05-29 18:40:46 +0530

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	
+	Scanner sc= new Scanner(System.in);
+	System.out.println("ENTER YOUR AGE  ");
+	int age=sc.nextInt();
+	
+	if (age>18){
+		System.out.println("YOU ARE ADULT");	
+
+		}
+	else{
+		System.out.println("YOU ARE NOT ADULT");
+		}
+	
+	}
+}
