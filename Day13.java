39f1436037b9eaef9b8e4e844348c0a0ea4cd688
het-codes-ai <modijignisha12@gmail.com>
2026-06-01 09:27:33 +0530

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("ENTER YOUR NUMBER  ");
+	int n =sc.nextInt();
+	int num=1;
+	for (int i=0;i<n;i++){
+			
+			for (int j =0;j<n;j++) {
+				System.out.print(num+"  ");
+				num++;
+				
+				
+				}
+			
+			System.out.println();
+			}
+			
+	}
+}
