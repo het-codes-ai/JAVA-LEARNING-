59f4e531be6fd917b358f03a938a204f785799e8
het-codes-ai <modijignisha12@gmail.com>
2026-06-01 09:45:55 +0530

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);	System.out.println("ENTER YOUR NUMBER  ");
+	int n=sc.nextInt();
+	char star='*';
+	for(int i=0;i<=n;i++){
+		
+		for(int j=0;j<i;j++){
+			System.out.print(star + "  ");
+			
+			
+			}
+		
+		System.out.println();
+		
+		}
+	}
+}
