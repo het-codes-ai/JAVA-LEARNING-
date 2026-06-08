ee5ad9c865ec021844b390c1ca5723c626b632e3
HET MODI <modijignisha12@gmail.com>
2026-06-08 21:00:30 +0530

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static int sumN(int a){
+		int sum=0;
+		for (int n=1;n<=a;n++){
+			
+			sum+=n;
+			
+			}
+		System.out.println(sum);
+			
+				
+		return 0;
+		}
+	public static void main(String[] args) {
+	sumN(4) ;
+	sumN(10) ;
+	}
+}
