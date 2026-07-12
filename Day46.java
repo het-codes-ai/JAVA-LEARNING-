b9223a3560c2b025f8e9f032514b58c5b2ee218e
HET MODI <modijignisha12@gmail.com>
2026-07-12 20:55:23 +0530

    Create Day46.java

---
+++ Day46.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int[][] matrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}} ;
+		int target = 5;
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] == target) {
+					System.out.println("<" + i + "," + j + ">");
+					return ;
+				}
+			}
+		}
+	}
+}
