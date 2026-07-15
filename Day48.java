d6ed88c53cdca5d3e6212b5d1ae5b81fba499936
HET MODI <modijignisha12@gmail.com>
2026-07-15 16:42:37 +0530

    Create Day48.java

---
+++ Day48.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void printNums(int n){
+		if(n==1){
+			System.out.println("1");
+			return;
+			}
+		System.out.println(n);		
+		printNums(n-1);				
+		}
+	public static void main(String[] args) {
+	printNums(5);	
+	}
+}
